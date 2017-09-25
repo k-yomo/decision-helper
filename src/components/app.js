5f34a26ef4cbbc81ba0eb95d5e25d4c863b3e290
Kanij Yomoda <kanji@mbp.gateway.sonic.net>
2017-09-24 23:53:08 -0700

    Add delete and making dicision buttons

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,12 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      title: 'Decision Helper',
       options: []
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onRemoveAll = this.onRemoveAll.bind(this);
+    this.onMakeDecision = this.onMakeDecision.bind(this);
   }
 
   onFormSubmit(e) {
@@ -14,18 +17,33 @@ export default class App extends Component {
 
     const option = e.target.elements.option.value;
     if (option) {
-      this.setState({options: this.state.options.concat(option)});
+      this.setState({ options: this.state.options.concat(option) });
       e.target.elements.option.value = '';
     } else {
       console.log(this.state.options);
     }
   }
 
+  onRemoveAll() {
+    this.setState({ options: [] });
+  }
+
+  onMakeDecision() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+
+  }
+
   render() {
     return (
       (
         <div>
-          <div>{this.state.options.map((option) => <p>{option}</p>)}</div>
+          <h1>{this.state.title}</h1>
+          <p>{this.state.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+          <ol>{this.state.options.map((option) => <li>{option}</li>)}</ol>
+          <button disabled={this.state.options.length === 0} onClick={this.onMakeDecision}>What should I do?</button>
+          <button onClick={this.onRemoveAll}>Remove All</button>
           <form onSubmit={this.onFormSubmit}>
             <input type="text" name="option" />
             <button>Add Option</button>
