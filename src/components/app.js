0400f231e5a3a1a8f4b5780352463aabd8248e3d
Kanij Yomoda <kanji@mbp.gateway.sonic.net>
2017-09-25 23:36:40 -0700

    Add Option component

--- src/components/app.js
+++ src/components/app.js
@@ -24,8 +24,16 @@ class Action extends Component {
 class Options extends Component {
   render() {
     return (
-      <div>Options component here</div>
+      <div>
+        <Option />
+      </div>
     );
+  }
+}
+
+class Option extends Component {
+  render() {
+    <div>Options component here</div>;
   }
 }
 
@@ -54,60 +62,3 @@ export default class App extends Component {
     );
   }
 }
-
-
-// import React, { Component } from 'react';
-//
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       title: 'Decision Helper',
-//       options: []
-//     };
-//     this.onFormSubmit = this.onFormSubmit.bind(this);
-//     this.onRemoveAll = this.onRemoveAll.bind(this);
-//     this.onMakeDecision = this.onMakeDecision.bind(this);
-//   }
-//
-//   onFormSubmit(e) {
-//     e.preventDefault();
-//
-//     const option = e.target.elements.option.value;
-//     if (option) {
-//       this.setState({ options: this.state.options.concat(option) });
-//       e.target.elements.option.value = '';
-//     } else {
-//       console.log(this.state.options);
-//     }
-//   }
-//
-//   onRemoveAll() {
-//     this.setState({ options: [] });
-//   }
-//
-//   onMakeDecision() {
-//     const randomNum = Math.floor(Math.random() * this.state.options.length);
-//     const option = this.state.options[randomNum];
-//     alert(option);
-//
-//   }
-//
-//   render() {
-//     return (
-//       (
-//         <div>
-//           <h1>{this.state.title}</h1>
-//           <p>{this.state.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-//           <ol>{this.state.options.map((option) => <li>{option}</li>)}</ol>
-//           <button disabled={this.state.options.length === 0} onClick={this.onMakeDecision}>What should I do?</button>
-//           <button onClick={this.onRemoveAll}>Remove All</button>
-//           <form onSubmit={this.onFormSubmit}>
-//             <input type="text" name="option" />
-//             <button>Add Option</button>
-//           </form>
-//         </div>
-//       )
-//     );
-//   }
-// }
