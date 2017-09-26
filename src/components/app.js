8d2e98e9e1bbdee3a953d775123d203ab9d2b35d
yomoda07 <kanji.yy@gmail.com>
2017-09-26 00:18:35 -0700

    Add button to add option

--- src/components/app.js
+++ src/components/app.js
@@ -4,28 +4,37 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <h1>Decision Helper</h1>
-        <h3>Put your life in the hands of a computer</h3>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.subtitle}</h3>
       </div>
     );
   }
 }
 
 class Action extends Component {
+  handlePick() {
+
+  }
+
   render() {
     return (
       <div>
-        <button>What shoud I do?</button>
+        <button onClick={this.handlePick}>What shoud I do?</button>
       </div>
     );
   }
 }
 
 class Options extends Component {
+  handleRemoveAll() {
+    alert("removeALl");
+  }
+
   render() {
     return (
       <div>
-        <Option />
+        <button onClick={this.handleRemoveAll}>Remove ALL</button>
+        {this.props.options.map(option => <Option key={option} optionText={option} />)}
       </div>
     );
   }
@@ -33,15 +42,29 @@ class Options extends Component {
 
 class Option extends Component {
   render() {
-    <div>Options component here</div>;
+    return (
+      <div>{this.props.optionText}</div>
+    );
   }
 }
 
 class AddOption extends Component {
 
+  handleAddOption(e) {
+    e.preventDefault();
+    if (e.target.option.value) {
+    alert(e.target.option.value);
+    }
+  }
+
   render() {
 	   return (
-       <div>AddOption Component here</div>
+       <div>
+         <form onSubmit={this.handleAddOption} >
+           <input type="text" name="option" />
+           <button>Add Option</button>
+         </form>
+      </div>
      );
   }
 }
@@ -52,11 +75,15 @@ export default class App extends Component {
   }
 
   render() {
+    const title = 'Decision Helper';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing three'];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
